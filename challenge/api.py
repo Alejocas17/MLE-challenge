d35d393d3826733a87a4f4579dc5110100ed7536
alejandro castillo <Alejocas17@hotmail.com>
2023-10-02 15:14:05 -0500

    first commit

---
+++ challenge/api.py
@@ -0,0 +1,17 @@
+import fastapi
+import uvicorn
+app = fastapi.FastAPI()
+
+@app.get("/health", status_code=200)
+async def get_health() -> dict:
+    return {
+        "status": "OK"
+    }
+
+@app.post("/predict", status_code=200)
+async def post_predict() -> dict:
+    return
+
+
+if __name__=='__main__':
+    uvicorn.run("api:app",port=7777,reload=True,host="localhost")
